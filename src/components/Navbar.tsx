51104ccade041e8524793ca2246fee8c0f8f8ba9
Leonel  Alvarenga Ceron <Leonel.Alvarenga1@Focusservices.com>
2026-01-29 10:36:55 -0600

    chore: Remove Explorar button from navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -70,19 +70,6 @@ export default function Navbar({ totalListings, onRefresh }: NavbarProps) {
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
                             </svg>
                         </button>
-
-                        {/* CTA Button */}
-                        <a
-                            href="#departamentos"
-                            className="btn-primary"
-                            style={{
-                                borderRadius: '999px',
-                                fontSize: '0.875rem',
-                                padding: '0.5rem 1rem'
-                            }}
-                        >
-                            Explorar
-                        </a>
                     </div>
                 </div>
             </div>
